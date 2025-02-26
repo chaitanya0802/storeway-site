799e1f9d73984dbbe36150d1277895184147a632
chaitanya0802 <chaitanya7447@gmail.com>
2025-02-26 12:10:02 +0530

    popup changes

--- script.js
+++ script.js
@@ -23,6 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// show intro pop as site is opened
+document.addEventListener("DOMContentLoaded", function () {
+    // Show the popup after 5 seconds
+    setTimeout(function () {
+        showPopup('intro');
+
+        // Hide the popup after 10 seconds (5s after it appears)
+        setTimeout(function () {
+            closePopup('intro');
+        }, 5500); // 5000ms = 5 seconds
+    }, 700); // 5000ms = 5 seconds
+});
+
+
+
 
 
 
